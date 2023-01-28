887d9ae5fdc1a73f0e1cf373496c804c95583cae
Robinee <yb6925@naver.com>
2023-01-28 20:53:35 +0900

    Add eventListener and set variable for button

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 //Create a webpage with a 16x16 grid of square divs
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        const singleDiv = document.createElement("div");
-        container.appendChild(singleDiv);
+window.addEventListener("DOMContentLoaded", setupDivs)
+
+function setupDivs(){
+    for(let i = 0; i < 16; i++){
+        for(let j = 0; j < 16; j++){
+            const singleDiv = document.createElement("div");
+            container.appendChild(singleDiv);
+        }
     }
+
+    const divs = container.querySelectorAll("div");
+
+    divs.forEach((div)=>{
+        div.addEventListener("mouseover", ()=>{
+            div.style.background = "red";
+        })
+    })
 }
+
+const newSettingBtn = document.querySelector(".pixel-size");
