2e024da18f457133b329bc77ca0986c5e840a7e4
Robinee <yb6925@naver.com>
2023-01-28 19:38:34 +0900

    Write 16 * 16 divs in container

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+//Create a webpage with a 16x16 grid of square divs
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++){
+        const singleDiv = document.createElement("div");
+        singleDiv.textContent = "div"
+        container.appendChild(singleDiv);
+    }
+}
