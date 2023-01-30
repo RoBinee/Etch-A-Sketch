51eda9e15ec975da9d7837da7ca14d6ffd770702
Robinee <yb6925@naver.com>
2023-01-30 15:11:36 +0900

    Delete setUpDivs func and edit css for dynamic grid

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 //Create a webpage with a 16x16 grid of square divs
 const container = document.querySelector(".container");
+let numOfDiv = 16;
 
-window.addEventListener("DOMContentLoaded", setupDivs)
+window.addEventListener("DOMContentLoaded", generateDivs(numOfDiv));
 
-function setupDivs(){
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", ()=>{
+    const size = parseInt(prompt("Set the number of squares. Limit is 100"));
+    //stypeof size is a string
+
+    if(size >= 100 || !size){
+        alert("Wrong input! size limit is 100 and type limit is number")
+    }else{
+        //go to the generate function
+        generateDivs(size)
+    }
+})
+
+function generateDivs(num){
+    container.innerHTML = ""
+
+
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j < num; j++){
             const singleDiv = document.createElement("div");
             container.appendChild(singleDiv);
         }
     }
+    container.style.gridTemplateColumns = `repeat(${num}, auto)`;
+    container.style.gridTemplateRows = `repeat(${num}, auto)`;
 
     const divs = container.querySelectorAll("div");
 
@@ -18,6 +39,5 @@ function setupDivs(){
             div.style.background = "red";
         })
     })
-}
 
-const newSettingBtn = document.querySelector(".pixel-size");
+}
