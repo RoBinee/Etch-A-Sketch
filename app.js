eb9955ce75c69279ce3341ef8331511f7882eba4
Robinee <yb6925@naver.com>
2023-01-31 12:16:13 +0900

    Edit resetBtn addEventListener

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const inputForSize = document.querySelector("#input-size");
 const sizeDesc = document.querySelector(".size-desc");
 const colorInput = document.querySelector("#color-picker");
 
-let pixelNum = 16;
+const startBtn = document.querySelector(".brush");
+const eraseBtn = document.querySelector(".eraser");
+const resetBtn = document.querySelector(".reset");
 
+let pixelNum = 16;
+let userColor = colorInput.value;
+
+// event listeners
 window.addEventListener("DOMContentLoaded", generateDivs(pixelNum));
 
 inputForSize.addEventListener("change", (e)=>{
@@ -12,6 +18,26 @@ inputForSize.addEventListener("change", (e)=>{
     sizeDesc.textContent = `${pixelNum} × ${pixelNum}`
     generateDivs(pixelNum)
 })
+
+colorInput.addEventListener("input", (e)=>{
+    userColor = e.currentTarget.value;
+})
+
+startBtn.addEventListener("click",()=>{
+    userColor = colorInput.value;
+    setDivEvent()
+})
+
+eraseBtn.addEventListener("click", ()=>{
+    userColor = "#ffffff"
+    setDivEvent();
+})
+
+resetBtn.addEventListener("click", ()=>{
+    generateDivs(pixelNum);
+})
+
+//functions
 
 function generateDivs(num){
     container.innerHTML = ""
@@ -24,17 +50,9 @@ function generateDivs(num){
     }
     container.style.gridTemplateColumns = `repeat(${num}, auto)`;
     container.style.gridTemplateRows = `repeat(${num}, auto)`;
-
-    //can draw before button is clicked
-    // setColorandEvent(colorInput.value);
 }
 
-colorInput.addEventListener("input", (e)=>{
-    const userColor = e.currentTarget.value;
-    // setColorandEvent(userColor);
-})
-
-function setColorandEvent(userColor){
+function setDivEvent(){
     const divs = container.querySelectorAll("div");
 
     divs.forEach((div)=>{
@@ -43,24 +61,3 @@ function setColorandEvent(userColor){
         })
     })
 }
-
-const startBtn = document.querySelector(".brush");
-const eraseBtn = document.querySelector(".eraser");
-const resetBtn = document.querySelector(".reset");
-
-startBtn.addEventListener("click",()=>{
-    setColorandEvent(colorInput.value)
-    //add event listener
-})
-
-eraseBtn.addEventListener("click", ()=>{
-    setColorandEvent("#ffffff");
-})
-
-resetBtn.addEventListener("click", ()=>{
-    const divs = container.querySelectorAll("div");
-
-    divs.forEach((div)=>{
-        div.style.background = "#ffffff"
-    })
-})
